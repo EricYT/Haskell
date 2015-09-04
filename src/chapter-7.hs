4e3bb8554c43d4ce523f73041b557c7e46e6a9f3
EricYT <eric.yutao@gmail.com>
2015-09-04 23:26:03 +0800

    test

--- src/chapter-7.hs
+++ src/chapter-7.hs
@@ -52,6 +52,11 @@ data Vector a = Vector a a a deriving (Show)
 vplus :: (Num a) => Vector a -> Vector a -> Vector a
 vplus (Vector i j k) (Vector l m n) = Vector (i+l) (j+m) (k+n)
 
+data Person1 = Person1 { first :: String
+                       , last :: String
+                       , agenew :: Int
+                       } deriving (Eq)
+
 
 
 
