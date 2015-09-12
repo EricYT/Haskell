4c8b29bd7d057ba97761450d1a7861fdaa313600
EricYT <eric.yutao@gmail.com>
2015-09-12 13:22:43 +0800

    chapter 11

---
+++ src/chapter-11-4.hs
@@ -0,0 +1,29 @@
+-- import Control.Applicative
+
+
+class (Functor f) => Applicative f where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
+
+instance Applicative Maybe where
+    pure = Just
+    Nothing <*> _ = Nothing
+    (Just f) <*> something = fmap f something
+
+instance Applicative [] where
+    pure x = [x]
+    fs <*> xs = [f x | f <- fs, x <- xs]
+
+instance Applicative IO where
+    pure = return
+    a <*> b = do
+      f <- a
+      x <- b
+      return $ f x
+
+instance Applicative ((->) r) where
+    pure x = (\_ -> x)
+    f <*> g = \x -> f x (g x)
+
+
+
